dcdb1659cc658c6dc64c9eea560c4b201d9beb12
mhassan1 <marc.j.hassan@gmail.com>
2022-12-27 21:29:58 -0500

    Exclude `swagger-editor` dependencies

--- scripts/clone-swagger-editor.js
+++ scripts/clone-swagger-editor.js
@@ -1,16 +1,20 @@
-const child_process = require('child_process')
+const { execFileSync } = require('child_process')
+const { cpSync, writeFileSync } = require('fs')
 
 const { tempDir, buildDir } = execEnv
 
 const version = '3.18.2'
 
-child_process.execFileSync('git', [
+execFileSync('git', [
   'clone', 'https://github.com/swagger-api/swagger-editor.git',
   '-b', `v${version}`,
   '--depth', 1,
   tempDir
 ])
 
-child_process.execFileSync('cp', [`${tempDir}/package.json`, `${buildDir}/package.json`])
-child_process.execFileSync('mkdir', ['-p', `${buildDir}/src/plugins`])
-child_process.execFileSync('cp', ['-r', `${tempDir}/src/plugins`, `${buildDir}/src/.`])
+writeFileSync(`${buildDir}/package.json`, JSON.stringify({
+  name: 'swagger-editor-git',
+  version
+}))
+
+cpSync(`${tempDir}/src/plugins`, `${buildDir}/src/plugins`, { recursive: true })
